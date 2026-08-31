564d45671e0c3c974a02be60d6eecb6b141d08d7
carlsalexandre <contatocarlosalexandrecb@gmail.com>
2026-08-31 17:40:55 -0300

    Hash passwords and validate emails
    This change adds email format validation and password hashing before creating users, using bcrypt to securely store hashed passwords. It also updates the SQL schema to support longer password hashes and includes the required dependencies for email validation and password security.

--- src/models/usuario.go
+++ src/models/usuario.go
@@ -1,9 +1,12 @@
 package models
 
 import (
+	"api/src/security"
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 type Usuario struct {
@@ -20,7 +23,9 @@ func (usuario *Usuario) Preparar(etapa string) error {
 		return erro
 	}
 
-	usuario.formatar()
+	if erro := usuario.formatar(etapa); erro != nil {
+		return erro
+	}
 	return nil
 }
 
@@ -37,15 +42,27 @@ func (usuario *Usuario) validar(etapa string) error {
 		return errors.New("É obrigatório um email.")
 	}
 
+	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
+		return errors.New("O e-mail é inválido.")
+	}
+
 	if etapa == "cadastro" && usuario.Senha == "" {
 		return errors.New("É obrigatório uma senha.")
 	}
-
 	return nil
 }
 
-func (usuario *Usuario) formatar() {
+func (usuario *Usuario) formatar(etapa string) error {
 	usuario.Nome = strings.TrimSpace(usuario.Nome)
 	usuario.Nick = strings.TrimSpace(usuario.Nick)
 	usuario.Email = strings.TrimSpace(usuario.Email)
+
+	if etapa == "cadastro" {
+		senhaComHash, erro := security.Hash(usuario.Senha)
+		if erro != nil {
+			return erro
+		}
+		usuario.Senha = string(senhaComHash)
+	}
+	return nil
 }
